3d7eebc7ae8a483d7a68f8bef1cdf05bf47f85a4
Yavuz <m.yavuzyagis@gmail.com>
2022-02-07 17:26:05 +0300

    Fix some bugs and add some features. THIS IS A GENERIC AUTOMATED Commit

--- www/index.js
+++ www/index.js
@@ -4,10 +4,9 @@ const pre = document.getElementById("wasmrust-canvas");
 const universe = Universe.new();
 
 const renderLoop = () => {
-    pre.textContent = universe.render();
-    universe.tick();
-  
-    requestAnimationFrame(renderLoop);
-  };
+  pre.textContent = universe.render();
+  universe.tick();
 
+  requestAnimationFrame(renderLoop);
+};
 requestAnimationFrame(renderLoop);
